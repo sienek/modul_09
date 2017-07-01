56019a172b00849f62986adc1b6bb776e241a719
sienek <daniel.r.sienkiewicz@gmail.com>
2017-07-01 20:59:36 +0200

    script.js with comments

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var name //deklaracje zmiennej
+name = prompt('Enter your name'); //wywołanie okienka wprowadzania wartości zmiennej "name"
+// alert('Hello, ' + name); // wyświetlenie okna z wartością zmiannej "name"
+console.log('Hello, ' + name); //wwyświetlenie wartości zmiennej "name" w konsoli
