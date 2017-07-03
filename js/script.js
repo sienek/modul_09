4396094ed8a411d9770ee57e0421acfca1e3bf41
sienek <daniel.r.sienkiewicz@gmail.com>
2017-07-03 12:23:48 +0200

    index.html and scripts.js updated - triangle area calculation formula

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-var name //deklaracje zmiennej
-name = prompt('Enter your name'); //wywołanie okienka wprowadzania wartości zmiennej "name"
-// alert('Hello, ' + name); // wyświetlenie okna z wartością zmiannej "name"
-console.log('Hello, ' + name); //wwyświetlenie wartości zmiennej "name" w konsoli
+function triangleArea() { //funcja obliczająca pole trójkąta
+	var
+	a = document.getElementById("triangleBase").value,
+	h = document.getElementById("traingleHeight").value,
+	area = 0.5*a*h;
+	document.getElementById("result").innerHTML = area; 
+	console.log(a); 
+	console.log(h);
+	console.log(area);
+}
+
+//obliczenie i wyświetlenie wyniku dla wartości początkowych po załadowaniu strony 
+//lub dla wartości bieżacych po przeładowaniu strony:
+triangleArea(); 
+
+//obliczenie i wyświetlenie wyniku po kliknięciu buttona "Oblicz":
+function clickTriangleArea() {
+triangleArea();
+}
+
+
+
