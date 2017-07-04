ebc6fee482b296b50ab20f6b67978c66276988d7
sienek <daniel.r.sienkiewicz@gmail.com>
2017-07-04 19:05:44 +0200

    formula result sign detection

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
-var name //deklaracje zmiennej
-name = prompt('Enter your name'); //wywołanie okienka wprowadzania wartości zmiennej "name"
-// alert('Hello, ' + name); // wyświetlenie okna z wartością zmiannej "name"
-console.log('Hello, ' + name); //wwyświetlenie wartości zmiennej "name" w konsoli
+function calculate() { //funcja obliczająca pole trójkąta
+	var
+	a = document.getElementById("a").value,
+	b = document.getElementById("b").value,
+	value = (a*a)+(2*a*b)-(b*b);
+	console.log(a); 
+	console.log(b);
+	console.log(value);
+
+
+	if (value>0) {
+		result="dodatnia.";
+	} else if (value<0) {
+		result="ujemna.";
+ 	} else {
+ 		result="równa zero.";
+ 	}
+document.getElementById("result").innerHTML = result; 
+	
+}
+
+function reset () {
+	a = document.getElementById("a").defaultValue;
+	b = document.getElementById("b").defaultValue;	
+}
+
+//obliczenie i wyświetlenie wyniku dla wartości początkowych po załadowaniu strony 
+//lub dla wartości bieżacych po przeładowaniu strony:
+calculate(); 
+
+
+
+
+
