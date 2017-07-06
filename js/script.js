99cf63a65869e2f1b4d838da0a72ea99503f0857
sienek <daniel.r.sienkiewicz@gmail.com>
2017-07-06 07:28:05 +0200

    function used

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-var name //deklaracje zmiennej
-name = prompt('Enter your name'); //wywołanie okienka wprowadzania wartości zmiennej "name"
-// alert('Hello, ' + name); // wyświetlenie okna z wartością zmiannej "name"
-console.log('Hello, ' + name); //wwyświetlenie wartości zmiennej "name" w konsoli
+function getTriangleArea(a, h) {
+	if ((a<=0)||(h<=0)) {
+		return "Nieprawidłowe dane";
+	}
+	else {
+		return "Pole trójkąta wynosi "+ 0.5*a*h +" cm"+"2".sup();
+	}
+}
+
+
+function display() {
+//console.log( getTriangleArea(document.getElementById("a").value,document.getElementById("h").value) );
+document.getElementById("result").innerHTML = getTriangleArea(document.getElementById("a").value,document.getElementById("h").value);
+}
