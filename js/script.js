f402c5d228bb75213a8936b2929019eff0799ffb
Nemethe <nowakmarceli@gmail.com>
2017-07-06 20:43:05 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
 function getTriangleArea(a, h) {
-	if ((a<=0)||(h<=0)) {
+	var aValue = parseFloat(a.value.replace(',', '.'));
+	var hValue = parseFloat(h.value.replace(',', '.'));
+
+	a.value = aValue;
+	h.value = hValue;
+
+	if (aValue<=0 || hValue<=0 ) {
 		return "Nieprawidłowe dane";
 	}
 	else {
-		return "Pole trójkąta wynosi "+ 0.5*a*h +" cm"+"2".sup();
+		return "Pole trójkąta wynosi "+ 0.5*aValue*hValue +" cm"+"2".sup();
 	}
 }
 
 
 function display() {
 //console.log( getTriangleArea(document.getElementById("a").value,document.getElementById("h").value) );
-document.getElementById("result").innerHTML = getTriangleArea(document.getElementById("a").value,document.getElementById("h").value);
+document.getElementById("result").innerHTML = getTriangleArea(document.getElementById("a"),document.getElementById("h"));
 }
+
+function isNumeric(n) { 
+      return !isNaN(parseFloat(n)) && isFinite(n); 
+}
+
+function validate() {
+	if (!isNumeric(this.value)) {
+		// this.value = parseFloat(this.value);
+		this.className = 'error';
+	} else {
+		this.className = '';
+	}
+}
+
+document.getElementById("a").onkeyup = validate;
