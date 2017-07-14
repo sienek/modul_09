d0ced158062aea4bb13122a90175f3d34f496fb6
sienek <daniel.r.sienkiewicz@gmail.com>
2017-07-14 07:35:01 +0200

    drawing tree function, parameter input on the page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-var name //deklaracje zmiennej
-name = prompt('Enter your name'); //wywołanie okienka wprowadzania wartości zmiennej "name"
-// alert('Hello, ' + name); // wyświetlenie okna z wartością zmiannej "name"
-console.log('Hello, ' + name); //wwyświetlenie wartości zmiennej "name" w konsoli
+ 
+function addSpaces(starsNr, branchNr){
+	var spacesNr = starsNr - branchNr - 1; //wyznacza licznę spacji w rzędzie
+//	console.log(spacesNr);
+	var spaces = '';
+	for (var i = 0; i < spacesNr; i++) { //tworzy rząd spacji
+		spaces = spaces + ' ';
+	};
+	//console.log(spaces);
+	return spaces;
+};
+
+function drawBranch(starsNr){
+	var stars = '*'
+	console.log(addSpaces(starsNr, -1) + stars); //rysuje 1 gwizdkę na szczycie
+	for (var k = 0; k < starsNr -1; k++) {
+		stars =stars + '**'; //dodaje po 2 gwiazdki w każdym rzędzie
+		console.log(addSpaces(starsNr, k) + stars); //rysuje spacje i rzędy gwiazdek
+		//	console.log(stars);
+	};
+	console.log(addSpaces(starsNr, -1) + 'M'); //rysuje "pien"
+};
+
+function drawTree(){
+	drawBranch(document.getElementById("name").value);
+}
+
