e380a990c65a7df60c6c4cfaea05e8ad6c0e001f
sienek <daniel.r.sienkiewicz@gmail.com>
2017-07-17 07:54:36 +0200

    text in button reading - code cleared

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
-//var name //deklaracje zmiennej
-//name = prompt('Enter your name'); //wywołanie okienka wprowadzania wartości zmiennej "name"
-// alert('Hello, ' + name); // wyświetlenie okna z wartością zmiannej "name"
-//console.log('Hello, ' + name); //wwyświetlenie wartości zmiennej "name" w konsoli
-
 
 var buttons = document.getElementsByClassName('button');
-console.log(buttons);
+//console.log(buttons);
 for (var i = 0; i < buttons.length; i++) {
 	var buttonText = buttons[i].innerText;
-	console.log(buttonText);
+	//console.log(buttonText);
 	var buttonNr = i + 1;
 	alert("Tekst w przycisku " + buttonNr + ": " + buttonText);
 };
