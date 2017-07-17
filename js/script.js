3f7b78df44b351c851616500aae2fea7710670ed
sienek <daniel.r.sienkiewicz@gmail.com>
2017-07-17 07:51:03 +0200

    text in button reading

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-var name //deklaracje zmiennej
-name = prompt('Enter your name'); //wywołanie okienka wprowadzania wartości zmiennej "name"
+//var name //deklaracje zmiennej
+//name = prompt('Enter your name'); //wywołanie okienka wprowadzania wartości zmiennej "name"
 // alert('Hello, ' + name); // wyświetlenie okna z wartością zmiannej "name"
-console.log('Hello, ' + name); //wwyświetlenie wartości zmiennej "name" w konsoli
+//console.log('Hello, ' + name); //wwyświetlenie wartości zmiennej "name" w konsoli
+
+
+var buttons = document.getElementsByClassName('button');
+console.log(buttons);
+for (var i = 0; i < buttons.length; i++) {
+	var buttonText = buttons[i].innerText;
+	console.log(buttonText);
+	var buttonNr = i + 1;
+	alert("Tekst w przycisku " + buttonNr + ": " + buttonText);
+};
