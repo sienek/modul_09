16bba068546f6c08c73b12994ee8e2beb89839c2
sienek <daniel.r.sienkiewicz@gmail.com>
2017-07-17 21:26:49 +0200

    adding elements to a list

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-var name //deklaracje zmiennej
-name = prompt('Enter your name'); //wywołanie okienka wprowadzania wartości zmiennej "name"
-// alert('Hello, ' + name); // wyświetlenie okna z wartością zmiannej "name"
-console.log('Hello, ' + name); //wwyświetlenie wartości zmiennej "name" w konsoli
+
+function addElement(){
+	var listLength = document.getElementsByTagName('li').length;//sprawdza bieżacą l-bę elementów listy
+	var element = document.createElement('li'); //tworzy element lu
+	//console.log(listLength);
+	element.innerHTML = 'Item ' + listLength; //tworzy zawartość nowego elementi li
+	list.appendChild(element); //dodaje stworzony element li
+}
+
+var list = document.getElementById('list'); //pobranie elementu
+//console.log(list);
+var add = document.getElementById('addElem'); //pobranie elementu
+//console.log(add);
+add.addEventListener('click', function() {
+	addElement() //nałożenie listernera wywołującego na click funkcję 'addElement()'
+});
