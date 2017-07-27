63be68dc0905a92ae600b46c0bd033308acc8dd3
Nemethe <nowakmarceli@gmail.com>
2017-07-27 20:46:07 +0200

    expand solution

--- js/script.js
+++ js/script.js
@@ -8,10 +8,11 @@ $(function(){
     indicatorItem.css({'list-style-type':'disc'}); //ustawienie 1-szej kulki kontrolki na pełną
 
 
-function changeSlide() {
+function changeSlide(msTime) {
+    msTime = msTime || 500; console.log(msTime);
     //slajdy:
-    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-    console.log('hop');
+    carouselList.animate({'marginLeft':-400}, msTime, moveFirstSlide);
+    // console.log('hop');
 
     //kontrolki:
     indicatorItem.css({'list-style-type':'circle'}); //ustawienie bieżacej kulki na pustą
@@ -36,18 +37,65 @@ function moveFirstSlide(){
 
 }
 
-    setInterval(changeSlide, 3000);
+function changeSlidePrev(msTime) {
+    msTime = msTime || 500;
+    //slajdy:
+    carouselList.css({'marginLeft':-400});
+    // 
+    // console.log('hop');
+
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    firstItem.before(firstItem);
+
+    carouselList.animate({'marginLeft': 0}, msTime);
+
+    //kontrolki:
+    indicatorItem.css({'list-style-type':'circle'}); //ustawienie bieżacej kulki na pustą
+    if (i == 0) {
+        indicatorItem = indicatorList.find("li:last");
+    } else {
+        indicatorItem = indicatorItem.prev();
+    }
+    indicatorItem.css({'list-style-type':'disc'}); //ustawienie następnej kulki na pełną
+    //console.log(indicatorItem);
+
+    i = i-1; 
+    if (i < 0) { //przejście na 1-szą kulkę 
+        i = 4;
+    };
+}
+
+    var interv = setInterval(changeSlide, 3000);
+
+    function clearIntervalForTime(msTime) {
+        msTime = msTime || 500;
+
+        clearInterval(interv);
+        setTimeout(function () {
+            interv = setInterval(changeSlide, 3000);
+        }, msTime)
+    }
 
 
     $(".carousel-indicator ul li").hover(function(){ //reakcja kulek na hover
         $(this).css({'cursor':'pointer'});
     });
 
-    $(".carousel-indicator ul").click(function(){  //reakcja kulki na click
-        //alert($(this).index);
+    $(".carousel-indicator ul li").click(function(){  //reakcja kulki na click
+        var movesCount = $(this).index() - i;
+
+        clearIntervalForTime(10000);
+
+        for (var x = 0; x < movesCount; x++) {
+            setTimeout(changeSlide, 10000 / movesCount * x, 10000 / movesCount);
+        }
     });
 
-
+    $('.js-prev').click(function () {
+        clearIntervalForTime();
+        changeSlidePrev();
+    });
 });
 
 
