32614a047a7697a9ff6373c958980eaf5e00bb35
sienek <daniel.r.sienkiewicz@gmail.com>
2017-07-27 07:09:03 +0200

    indicators ok

--- js/script.js
+++ js/script.js
@@ -1,4 +1,43 @@
-var name //deklaracje zmiennej
-name = prompt('Enter your name'); //wywołanie okienka wprowadzania wartości zmiennej "name"
-// alert('Hello, ' + name); // wyświetlenie okna z wartością zmiannej "name"
-console.log('Hello, ' + name); //wwyświetlenie wartości zmiennej "name" w konsoli
+
+
+$(function(){
+    var carouselList = $("#carousel ul");
+    var indicatorList = $(".carousel-indicator ul");
+    var indicatorItem = indicatorList.find("li:first");
+    var i = 0;
+    indicatorItem.css({'list-style-type':'disc'});
+
+
+function changeSlide() {
+    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+    console.log('hop');
+
+    indicatorItem.css({'list-style-type':'circle'});
+    indicatorItem = indicatorItem.next();
+    indicatorItem.css({'list-style-type':'disc'});
+    //console.log(indicatorItem);
+
+    i = i+1;
+    if (i == 5) {
+        indicatorItem.css({'list-style-type':'circle'});
+        indicatorItem = indicatorList.find("li:first");
+        indicatorItem.css({'list-style-type':'disc'});
+        i = 0;
+    };
+} 
+
+function moveFirstSlide(){
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+
+}
+
+setInterval(changeSlide, 3000);
+
+
+});
+
+
+
