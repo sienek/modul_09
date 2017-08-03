6b5e2fcc4105551c7b764506ad39a79c58fe6a79
sienek <daniel.r.sienkiewicz@gmail.com>
2017-08-03 08:01:43 +0200

    buttons creation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,37 @@
-var name //deklaracje zmiennej
-name = prompt('Enter your name'); //wywołanie okienka wprowadzania wartości zmiennej "name"
-// alert('Hello, ' + name); // wyświetlenie okna z wartością zmiannej "name"
-console.log('Hello, ' + name); //wwyświetlenie wartości zmiennej "name" w konsoli
+function Button(text) {
+    this.text = text || 'Hello';
+}
+
+Button.prototype = {
+    create: function() {
+        var self = this;
+        this.$element = $('<button>');
+        this.$element.text(this.text);
+        this.$element.click(function() {
+            alert(self.text);
+        });
+        this.$element.appendTo($('.container'));
+    }
+}
+
+var btn1 = new Button('Hello!');
+btn1.create();
+var btn2 = new Button();
+btn2.create();
+var btn3 = new Button('OK');
+btn3.create();
+
+
+var btn4 = new Button('Hello!');
+btn4.create();
+var btn4 = new Button();
+btn4.create();
+var btn4 = new Button('OK');
+btn4.create();
+
+
+
+
+
+
+
