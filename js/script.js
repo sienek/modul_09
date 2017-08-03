647cc2e1adc30b0eeb8cc6455e1b336e0a36ad48
sienek <daniel.r.sienkiewicz@gmail.com>
2017-08-03 20:58:21 +0200

    remove function attempt

--- js/script.js
+++ js/script.js
@@ -7,12 +7,17 @@ Button.prototype = {
         var self = this;
         this.$element = $('<button>');
         this.$element.text(this.text);
-        this.$element.click(function() {
-            alert(self.text);
-        });
         this.$element.appendTo($('.container'));
+        this.$element.click(function() {
+            //alert(self.text);
+            this.$element.remove();
+            
+        });
+        //this.$element.remove();
     }
 }
+
+
 
 var btn1 = new Button('Hello!');
 btn1.create();
@@ -22,12 +27,8 @@ var btn3 = new Button('OK');
 btn3.create();
 
 
-var btn4 = new Button('Hello!');
-btn4.create();
-var btn4 = new Button();
-btn4.create();
-var btn4 = new Button('OK');
-btn4.create();
+
+
 
 
 
